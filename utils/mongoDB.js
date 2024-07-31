ec60841296a4d684ddeefc060ca75b75ea8c7367
abusayedwd <abusayednalita1234@gmail.com>
2024-07-31 09:24:26 +0600

    mongodb atlas added

--- utils/mongoDB.js
+++ utils/mongoDB.js
@@ -5,9 +5,17 @@
 export const mongoDb = async () => {
     try{
         await mongoose.connect(process.env.DB_url)
-        console.log('connent DB');
+        console.log('connent DB atlas');
     }
     catch(error){
         console.log(error);
     }
 }
+
+
+// DB_url = mongodb+srv://abusayed:cmuqR8YB8DFatfyo@cluster0.nwoisw1.mongodb.net/Register
+// NEXTAUTH_SECRET =  dfdfddgfgrdffdfrgfrrtrg
+// AUTH_URL = https://news-today-chi.vercel.app/
+
+
+// # cmuqR8YB8DFatfyo
